5a343f4dfaad5fc248334aca6e66a141412f27cc
Ramon Nunes <ramonkjl@gmail.com>
2021-02-04 19:47:04 -0300

    Foi criado paginá de post especifico

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose');
 const app = express();
 const admin = require('./routes/admin');
 const path = require('path');
-const session  = require('express-session');
+const session = require('express-session');
 const flash = require('connect-flash');
+require('./models/Postagem');
+const Postagem = mongoose.model("Postagem");
 
 
 //COFIGURAÇÕES
@@ -18,7 +20,7 @@ app.use(session({
 }))
 app.use(flash())//FLASH
 //MIDDLEWERE
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.locals.success_msg = req.flash("success_msg") //variavel global acessada em qualquer pagina
     res.locals.error_msg = req.flash("error_msg")
     next();
@@ -36,10 +38,11 @@ app.set('view engine', 'handlebars');
 
 //MONGOOSE
 mongoose.connect('mongodb+srv://ramon_teste:32251049@ramon.lxiex.mongodb.net/ramon_teste_mongo?retryWrites=true&w=majority', {
-     useNewUrlParser: true, 
-     useUnifiedTopology: true,
-    useFindAndModify: false })
-     .then(() => console.log("Conectado ao mongo"))
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+    .then(() => console.log("Conectado ao mongo"))
     .catch((err) => console.log("Erro ao se conectar", err))
 
 //DIRETÓRIO PUBLIC
@@ -49,9 +52,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //ROTAS
-app.get('/', (req, res)=>{
-    res.render('index')
+app.get('/', (req, res) => {
+    Postagem.find().lean()
+        .populate("categoria").sort({ data: 'desc' })
+        .then((postagens) => {
+            res.render('index', { postagens, postagens })
+        })
+        .catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect('/404');
+        })
 })
+
+app.get('/postagem/:slug', (req, res)=>{
+    Postagem.findOne({slug: req.params.slug}).lean()
+    .then((postagem)=>{
+        if(postagem){
+            res.render('postagem/index', {postagem: postagem})
+
+        }else{
+            req.flash("error_msg", "Essa postagem não existe")
+            res.redirect('/')
+        }
+    })
+    .catch((err)=>{
+        req.flash("error_msg", "Houve um erro interno")
+        res.redirect('/')
+    })
+})
+
+app.get('/404', (req, res) => { res.send("Error 404") })
 
 app.use('/admin', admin);
 
