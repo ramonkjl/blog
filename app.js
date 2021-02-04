5cc131363607badecfc3f3832804a97610954d2b
Ramon Nunes <ramonkjl@gmail.com>
2021-02-04 02:10:43 -0300

    Configurando MongoDB e rota de post para salvar categorias, também foi criado middlewere

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const app = express();
 const admin = require('./routes/admin');
 const path = require('path');
 
 
 //COFIGURAÇÕES
-    //BODY-PARSER
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
+//BODY-PARSER
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-    //HANDLEBARS
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}));
-    app.set('view engine', 'handlebars');
+//HANDLEBARS
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-    //MONGOOSE
+//MONGOOSE
+mongoose.connect('mongodb+srv://ramon_teste:32251049@ramon.lxiex.mongodb.net/ramon_teste_mongo?retryWrites=true&w=majority', {
+     useNewUrlParser: true, 
+     useUnifiedTopology: true })
+     .then(() => console.log("Conectado ao mongo"))
+    .catch((err) => console.log("Erro ao se conectar", err))
 
-    //DIRETÓRIO PUBLIC
-    app.use(express.static(path.join(__dirname, 'public')));
+//DIRETÓRIO PUBLIC
+app.use(express.static(path.join(__dirname, 'public')));
+
+//MIDDLEWERE
+app.use((req, res, next)=>{
+    console.log("OI EU SOU UM MIDDLEWERE")
+    next();
+})
 
 
 //ROTAS
@@ -27,4 +38,4 @@ app.use('/admin', admin);
 
 //OUTROS
 const PORT = 8081;
-app.listen(PORT, ()=> console.log(`Server runing in port ${PORT}`));
+app.listen(PORT, () => console.log(`Server runing in port ${PORT}`));
--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/Categoria');
+const Categoria = mongoose.model("Categoria");
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
     res.render("admin/index");
 });
 
-router.get('/posts', (req, res)=>{
-    
+router.get('/posts', (req, res) => {
+
 });
 
-router.get('/categorias', (req, res)=>{
+router.get('/categorias', (req, res) => {
     res.render("admin/categorias");
 });
 
-router.get('/categorias/add', (req, res)=>{
+router.get('/categorias/add', (req, res) => {
     res.render("admin/addcategorias");
 })
 
-module.exports = router;
+//Rota de post
+//Aqui uso uma async
+router.post('/categorias/nova', (req, res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug,
+    }
+
+    async function salvar() {
+        await new Categoria(novaCategoria).save()
+            .then(() => console.log("Categoria Salva com Sucesso"))
+            .catch((err) => console.log("erro ao registrar categoria"))
+    }
+
+    salvar();
+    res.render('admin/categorias');
+
+})
+module.exports = router
